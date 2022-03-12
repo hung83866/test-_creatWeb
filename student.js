7840201f3a546a4c97a303c2eeaf513a0e4c63c3
unknown <hung.pnpt93@gmail.com>
2022-03-12 12:40:13 +0700

    lan 1

---
+++ student.js
@@ -0,0 +1,29 @@
+class Student{
+    constructor(id, name, age) {
+        this.id = id;
+        this.name = name ;
+        this. age = age;
+
+    }
+
+    getId(){
+        return this.id;
+    }
+    setId(newId){
+        this.id = newId;
+    }
+    getName(){
+        return this.name;
+    }
+    setId(newName){
+        this.name = newName;
+    }
+    getAge(){
+        return this.age;
+    }
+    setAge(newAge){
+        this.age = newAge;
+    }
+
+
+}
